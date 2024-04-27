0fccf680ca93148dd2271837f862e2c97322e8cd
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2024-04-28 03:46:20 +0530

    Store restaurantId in local

--- apps/customer/src/common/components/common-layout.tsx
+++ apps/customer/src/common/components/common-layout.tsx
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 import BottomTabs from "./bottom-tabs";
 import Navbar from "./navbar";
+import { useEffect } from "react";
 
 const CommonLayout = ({ showBottomTabs = true }: { showBottomTabs?: boolean }) => {
+  const [sp] = useSearchParams();
+  const rid = sp.get("rid");
+
+  useEffect(() => {
+    if (rid) {
+      localStorage.setItem("restaurantId", rid);
+    }
+  }, [rid]);
+
   return (
     <main className="bg-gray-50 dark:bg-[#181a20]">
       <Navbar />
