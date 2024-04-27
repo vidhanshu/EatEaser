251c6db1232803402a22f1f2e1716127c0b827bf
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2024-04-28 03:32:16 +0530

    Forget to change directory in previous commit while commiting

--- apps/api/src/routes/customer/restaurant/category.route.ts
+++ apps/api/src/routes/customer/restaurant/category.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { ROUTES } from "../../../utils/routes";
 import { commonCategoryController } from "../../../controllers";
-import { RBACMiddleware, authMiddleware, validate } from "../../../middlewares";
+import { validate } from "../../../middlewares";
 import {
   commonValidation,
   customerCategoryValidation,
@@ -11,15 +11,11 @@ export const customerCategoryRouter = Router();
 
 customerCategoryRouter.get(
   ROUTES.customer.restaurant.category.byId,
-  authMiddleware,
-  RBACMiddleware(["customer"]),
   validate(commonValidation.idParamPayloadSchema),
-  commonCategoryController.handleGetCategoryById("customer"),
+  commonCategoryController.handleGetCategoryById("customer")
 );
 customerCategoryRouter.get(
   ROUTES.customer.restaurant.category.list,
-  authMiddleware,
-  RBACMiddleware(["customer"]),
   validate(customerCategoryValidation.listCategoriesSchema),
-  commonCategoryController.handleListCategories("customer"),
+  commonCategoryController.handleListCategories("customer")
 );
--- apps/api/src/routes/customer/restaurant/menuItem.route.ts
+++ apps/api/src/routes/customer/restaurant/menuItem.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { RBACMiddleware, authMiddleware, validate } from "../../../middlewares";
+import { validate } from "../../../middlewares";
 import { ROUTES } from "../../../utils/routes";
 import {
   commonValidation,
@@ -11,15 +11,11 @@ export const customerMenuItemRouter = Router();
 
 customerMenuItemRouter.get(
   ROUTES.customer.restaurant.menuItem.list,
-  authMiddleware,
-  RBACMiddleware(["customer"]),
   validate(menuItemValidation.listMenuItemsSchema),
   commonMenuItemController.handleListItems("customer")
 );
 customerMenuItemRouter.get(
   ROUTES.customer.restaurant.menuItem.byId,
-  authMiddleware,
-  RBACMiddleware(["customer"]),
   validate(commonValidation.idParamPayloadSchema),
   commonMenuItemController.handleGetItemById("customer")
 );
