db633d296a3ebff24c7001da2bff7124f263a81c
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2024-04-30 16:43:00 +0530

    Update base url

--- apps/customer/src/common/utils/constants.ts
+++ apps/customer/src/common/utils/constants.ts
@@ -4,8 +4,8 @@ import { RiSearchFill, RiSearchLine } from "react-icons/ri";
 import { IoSettings, IoSettingsOutline } from "react-icons/io5";
 import { MenuPage, SearchPage, CartPage, SettingsPage } from "@src/common/utils/pages";
 
-// export const BASE_URL = "https://eateaser.onrender.com/api";
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://eateaser.onrender.com/api";
+// export const BASE_URL = "http://localhost:4000/api";
 
 export const BOTTOM_TABS = [
   {
